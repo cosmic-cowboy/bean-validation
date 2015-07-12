62a08a18f20ea002473d70808f6ed68e0c12f0b3
hiroo takaku <slgerkamp@gmail.com>
2015-07-12 13:20:24 +0900

    Bean Validationの動作確認：様々なバリデーション

--- bean-validation/src/main/java/com/slgerkamp/bean/validation/Customer.java
+++ bean-validation/src/main/java/com/slgerkamp/bean/validation/Customer.java
@@ -1,14 +1,26 @@
 package com.slgerkamp.bean.validation;
 
+import javax.validation.constraints.AssertFalse;
 import javax.validation.constraints.AssertTrue;
+import javax.validation.constraints.Max;
+import javax.validation.constraints.Min;
 
 public class Customer {
 
 	@AssertTrue
-	public final Boolean isExecutive;
+	public final Boolean hasChallengeSpirit;
 
-	public Customer(Boolean isExecutive) {
-		this.isExecutive = isExecutive;
+	@AssertFalse
+	public final Boolean hasCar;
+
+	@Min(0)
+	@Max(200)
+	public final int age;
+  
+	public Customer(Boolean hasChallengeSpirit, Boolean hasCar, int age) {
+		this.hasChallengeSpirit = hasChallengeSpirit;
+		this.hasCar = hasCar;
+		this.age = age;
 	}
 	
 }
--- bean-validation/src/test/java/com/slgerkamp/bean/validation/ValidationTest.java
+++ bean-validation/src/test/java/com/slgerkamp/bean/validation/ValidationTest.java
@@ -1,5 +1,8 @@
 package com.slgerkamp.bean.validation;
 
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
+
 import java.util.Set;
 
 import javax.validation.ConstraintViolation;
@@ -7,35 +10,62 @@ import javax.validation.Validation;
 import javax.validation.Validator;
 import javax.validation.ValidatorFactory;
 
-import org.junit.Test;
-import static org.junit.Assert.assertThat;
-import static org.hamcrest.CoreMatchers.*;
+import org.junit.experimental.runners.Enclosed;
+import org.junit.experimental.theories.DataPoints;
+import org.junit.experimental.theories.Theories;
+import org.junit.experimental.theories.Theory;
+import org.junit.runner.RunWith;
 
+@RunWith(Enclosed.class)
 public class ValidationTest {
 
-	@Test
-	public void エグゼクティブ以外は参加できない(){
-		ValidatorFactory validatorFactory = Validation.buildDefaultValidatorFactory();
-		Validator validator = validatorFactory.getValidator();
+	
+	@RunWith(Theories.class)
+	public static class 顧客のバリデーション{
+
+		@DataPoints
+		public static Fixture[] FIXTURES = {
+			new Fixture(new Customer(true, false, 40), 0, "あなたはチャレンジ精神があり、車を持っていません。"),
+			new Fixture(new Customer(false, true, 40), 2, "あなたはチャレンジ精神がなく、車を持っています。"),
+			new Fixture(new Customer(true, false, -1), 1, "生まれてきたら登録ください。"),
+			new Fixture(new Customer(true, false, 201), 1, "想定外の年齢です。")
+		};
 		
-		Customer customer = new Customer(false);
-		
-		Set<ConstraintViolation<Customer>> violations = validator.validate(customer);
-		assertThat("あなたはエグゼクティブではないです。", violations.size(), is(1));
-		
-		violations.stream().forEach(violation -> System.out.println(violation.getMessage()));
+		@Theory
+		public void チェック(Fixture f){
+
+			ValidatorFactory validatorFactory = Validation.buildDefaultValidatorFactory();
+			Validator validator = validatorFactory.getValidator();
+			Set<ConstraintViolation<Customer>> violations = validator.validate(f.customer);
+			assertThat(f.description, violations.size(), is(f.violationSize));
+
+			if (violations.size() > 0) {
+				violations.stream().forEach(violation -> System.out.println(violation.getMessage()));				
+			}
+		}
 	}
 	
-	@Test
-	public void エグゼクティブが参加する(){
-		ValidatorFactory validatorFactory = Validation.buildDefaultValidatorFactory();
-		Validator validator = validatorFactory.getValidator();
-		
-		Customer customer = new Customer(true);
-		
-		Set<ConstraintViolation<Customer>> violations = validator.validate(customer);
-		assertThat("あなたはエグゼクティブです。", violations.size(), is(0));
-		
-	}
+	
+	
+	static class Fixture {
 
+		final Customer customer;
+		final int violationSize;
+		final String description;
+		
+		public Fixture(Customer customer, int violationSize, String description) {
+			this.customer = customer;
+			this.violationSize = violationSize;
+			this.description = description;
+		}
+
+		@Override
+		public String toString() {
+			return String.format(
+					"when customer.hasChallengeSpirit =%s, customer.hasCar =%s, customer.age =%s, " + 
+					"violationSize=%s, description=%s", 
+					customer.hasChallengeSpirit, customer.hasCar, customer.age, 
+					violationSize, description);
+		}
+	}
 }
